d6b1e0b33c38c020d3fe5dce0cc0a9fdf778eb32
Ryan Dooley <r1d8o0o9@gmail.com>
2024-07-25 09:44:04 -0400

    add skeletons for backend tests for GS

---
+++ laravel-back/tests/Feature/ApiControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ApiControllerTest extends TestCase
+{
+    //logging in
+    public function test_a_user_can_submit_info_to_login(): void
+    {
+
+    }
+
+    public function test_incorrect_credentials_get_rejected(): void
+    {
+
+    }
+
+    //registering
+    public function test_a_user_can_register_a_new_account(): void
+    {
+
+    }
+
+    public function test_a_user_cannot_register_a_invalid_account(): void
+    {
+
+    }
+
+    //others
+    public function test_private_page_is_inaccessible_without_bearer(): void
+    {
+
+    }
+
+    public function test_private_page_is_accessible_with_bearer(): void
+    {
+
+    }
+
+    public function test_public_page_returns_payload(): void
+    {
+
+    }
+}
+
+
