6255080145f603e931786f90a354bb618c27ea8a
Ryan <r1d8o0o9@gmail.com>
2024-08-07 10:45:46 -0230

    move other password reset methods inside controller

--- laravel-back/app/Http/Controllers/AuthController.php
+++ laravel-back/app/Http/Controllers/AuthController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -20,5 +24,37 @@ class AuthController extends Controller
         return $status === Password::RESET_LINK_SENT
             ? back()->with(['status' => __($status)])
             : back()->withErrors(['email' => __($status)]);
+    }
+
+    public function redirectBack(string $token)
+    {
+        return redirect("http://localhost:3000/reset-password?token=$token");
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8',     //|confirmed
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password', 'token'),
+
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? redirect()->route('http://localhost:3000/login')->with('status', __($status))
+            : back()->withErrors(['email' => [__($status)]]);
     }
 }
--- laravel-back/routes/api.php
+++ laravel-back/routes/api.php
@@ -22,40 +22,16 @@ Route::post('/register', [ApiController::class, 'registerUser'])->name('register
 Route::post('/login', [ApiController::class, 'loginUser'])->name('loginUser');
 
 
+
+
+
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])
     ->middleware('guest')->name('password.email');
 
+Route::get('/reset-password/{token}', [AuthController::class, 'redirectBack'])
+    ->middleware('guest')->name('password.reset');
 
-Route::get('/reset-password/{token}', function (string $token) {
-
-    return redirect("http://localhost:3000/reset-password?token=$token");
-
-})->middleware('guest')->name('password.reset');
-
-Route::post('/reset-password', function (Request $request) {
-    $request->validate([
-        'token' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|min:8',     //|confirmed
-    ]);
-
-    $status = Password::reset(
-        $request->only('email', 'password', 'password', 'token'),
-
-        function (User $user, string $password) {
-            $user->forceFill([
-                'password' => Hash::make($password)
-            ])->setRememberToken(Str::random(60));
-
-            $user->save();
-
-            event(new PasswordReset($user));
-        }
-    );
-
-    return $status === Password::PASSWORD_RESET
-        ? redirect()->route('http://localhost:3000/login')->with('status', __($status))
-        : back()->withErrors(['email' => [__($status)]]);
-})->middleware('guest')->name('password.update');
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])
+    ->middleware('guest')->name('password.update');
 
 
